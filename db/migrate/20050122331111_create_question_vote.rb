6673a4a44b0c2f79f5d71c79c4e571c392e00813
Cheng Yin <Crystal@Xus-MacBook-Pro.local>
2016-07-28 10:28:43 +0800

    fixed minor bug for QuestionVote table

--- db/migrate/20050122331111_create_question_vote.rb
+++ db/migrate/20050122331111_create_question_vote.rb
@@ -1,7 +1,7 @@
 class CreateQuestionVote < ActiveRecord::Migration
   def change
 	  create_table :question_votes do |t|
-    	t.string :vote
+    	t.integer :vote
     	t.belongs_to :question, index: true
     	t.belongs_to :user, index: true
       t.timestamps
